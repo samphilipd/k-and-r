d5429ab0e084e40848a3b6b42a7abc9bf9c59300
Sam Davies <seivadmas@gmail.com>
2015-08-07 19:27:52 -0300

    1.21

--- exercises/1_20.c
+++ exercises/1_20.c
@@ -3,17 +3,17 @@
 // detab: replace tabs with a specified number of spaces
 int main()
 {
-        int tab_width = 8;
-        char c;
+	int tab_width = 8;
+	char c;
 
-        while ((c = getchar()) != EOF) {
-                if (c == '\t')
-                        for (int i = 0; i < tab_width; ++i)
-                                putchar(' ');
-                else
-                        putchar(c);
-        }
+	while ((c = getchar()) != EOF) {
+		if (c == '\t')
+			for (int i = 0; i < tab_width; ++i)
+				putchar(' ');
+		else
+			putchar(c);
+	}
 
-        return 0;
+	return 0;
 
 }
---
+++ exercises/1_21.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// entab: replaces strings of blanks with tabs and any extra spaces necessary
+int main()
+{
+        unsigned int tab_width = 8; // this value is effectively ignored if set to < 2
+        int sequential_blank_count = 0;
+        char c;
+
+
+        while ((c = getchar()) != EOF) {
+                if (c == ' ') {
+                        sequential_blank_count++;
+                        if (sequential_blank_count >= 2 && sequential_blank_count == tab_width) {
+                                putchar('\t');
+                                sequential_blank_count = 0;
+                        }
+                } else {
+                        for (int i = 0; i < sequential_blank_count; ++i)
+                                putchar(' ');
+                        sequential_blank_count = 0;
+                        putchar(c);
+                }
+        }
+
+        return 0;
+
+}
